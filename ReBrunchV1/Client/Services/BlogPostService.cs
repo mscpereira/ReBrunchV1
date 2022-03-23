ed90832254171e1bf56f55b1c5c557a7eb4e1292
Mário Pereira <marioscpereira@gmail.com>
2022-03-23 01:41:21 +0000

    Created BlogPostService with and interface IBlogPostService and implementation class and moved de hard coded BlogPosts from razor component. Created two methods GetBLogPosts() and GetBlogPostByUrl(). Registred BlogPostService in Program.cs and now using dependency injection. Also, created links from index posts to each post show.

---
+++ ReBrunchV1/Client/Services/BlogPostService.cs
@@ -0,0 +1,25 @@
+﻿using ReBrunchV1.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReBrunchV1.Client.Services
+{
+    public class BlogPostService : IBlogPostService
+    {
+        public List<BlogPost> Posts { get; set; } = new List<BlogPost>()
+        {
+            new BlogPost { Id = 1, Url = "first-post", DateCreated = new DateTime(2021, 10, 01), Restaurant = "Canto dos Sabores", Title= "Our first review, a magical experience!", Description ="This is our very first Post. From the eggs to the bread, all perfect."},
+            new BlogPost { Id = 2, Url = "second-post", DateCreated = new DateTime(2022, 02, 22), Restaurant = "Pátio dos Petiscos", Title= "Our second review, a bad experience!", Description ="This is our second experience. From the milk to the ham, low on both quality and quantity."}
+        };
+        public BlogPost GetBlogPostByUrl(string url)
+        {
+            return Posts.FirstOrDefault(p => p.Url.ToLower().Equals(url.ToLower()));
+        }
+
+        public List<BlogPost> GetBlogPosts()
+        {
+            return Posts;
+        }
+    }
+}
---
+++ ReBrunchV1/Client/Services/IBlogPostService.cs
@@ -0,0 +1,11 @@
+﻿using ReBrunchV1.Shared;
+using System.Collections.Generic;
+
+namespace ReBrunchV1.Client.Services
+{
+    interface IBlogPostService
+    {
+        List<BlogPost> GetBlogPosts();
+        BlogPost GetBlogPostByUrl(string url);
+    }
+}
